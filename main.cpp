4ea57862c15909f1af075f91cf9a1f9de383924e
armsting <armsting@gmail.com>
2020-07-18 23:54:02 +0300

    Тест

--- main.cpp
+++ main.cpp
@@ -3,16 +3,23 @@
 #include <device.h>
 #include <QtSerialPort/QSerialPort>
 #include <QtSerialPort/QSerialPortInfo>
+#include "libfptr10.h"
 
 int main(int argc, char *argv[])
 {
     QVector<QString> list;
 
     getComPortList(&list);
-    std::cout<<"Привет\n";
+
+    libfptr_handle fptr;
+    libfptr_create(&fptr);
+
+    std::cout<<libfptr_get_version_string()<<"\n";
 
     /* QApplication a(argc, argv);
     MainWindow w;
     w.show();
     return a.exec();*/
+
+    libfptr_destroy(&fptr);
 }
