5b657431644ad2b61affebe6b509f00dade48d0e
armsting <armsting@gmail.com>
2020-07-30 07:32:38 +0300

    Разобрался с преобразованием широких символов в int

--- device.cpp
+++ device.cpp
@@ -30,22 +30,40 @@ bool validationINN(const std::wstring &CashierINN){
 
         return false;
     }
-    /*
-    int controlSum_1 = 0, controlSum_2 = 0;
+
+    int controlSum_1 = 0, controlSum_2 = 0; //Переменные для вычисления контрольной цифры
+
 
     if (CashierINN.length() == 10){
-        controlSum_1 = (int)CashierINN[0] * 2 + (int)CashierINN[1] * 4 + (int)CashierINN[2] * 10 +
-                (int)CashierINN[3] * 3 + (int)CashierINN[4] * 5 + (int)CashierINN[5] * 9 +
-                (int)CashierINN[6] * 4 + (int)CashierINN[7] * 6 + (int)CashierINN[8] * 8;
+        controlSum_1 = (CashierINN[0] - L'0')* 2 + (CashierINN[1] - L'0') * 4 + (CashierINN[2] - L'0') * 10 +
+                (CashierINN[3] - L'0') * 3 + (CashierINN[4] - L'0') * 5 + (CashierINN[5] - L'0') * 9 +
+                (CashierINN[6] - L'0') * 4 + (CashierINN[7] - L'0') * 6 + (CashierINN[8] - L'0') * 8;
 
         controlSum_2 = controlSum_1 / 11;
 
         controlSum_2 *= 11;
 
-        if(std::abs(controlSum_1 - controlSum_2) != (int)CashierINN[9]){
-
+        if(std::abs(controlSum_1 - controlSum_2) != CashierINN[9] - L'0'){
             return false;
-        }*/
-    return true;
+        }
     }
+
+    if (CashierINN.length() == 12){
+        controlSum_1 = (CashierINN[0] - '0')* 7 + (CashierINN[1] - '0') * 2 + (CashierINN[2] - '0') * 4 +
+                (CashierINN[3] - '0') * 10 + (CashierINN[4] - '0') * 3 + (CashierINN[5] - '0') * 5 +
+                (CashierINN[6] - '0') * 9 + (CashierINN[7] - '0') * 4 + (CashierINN[8] - '0') * 6 +
+                (CashierINN[9] - '0') * 8;
+
+        controlSum_2 = controlSum_1 / 11;
+
+        controlSum_2 *= 11;
+
+        if(std::abs(controlSum_1 - controlSum_2) != CashierINN[9] - '0'){
+            return false;
+        }
+    }
+
+    return true;
+}
+
 }
--- device.h
+++ device.h
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <codecvt>
 #include <locale>
+#include <bits/stdc++.h>
 
 namespace Device {
 
