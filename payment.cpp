4284028da80d89c9cad5be7e624c71aa7302a71c
ua.lukyanchuk <ua.lukyanchuk@tensor.ru>
2020-08-07 09:30:10 +0300

    Работа над формой оплаты

--- payment.cpp
+++ payment.cpp
@@ -22,11 +22,6 @@ Payment::~Payment()
     delete ui;
 }
 
-void Payment::on_pushButton_pay_clicked()
-{
-
-}
-
 void Payment::getPaymentSum(double _paymentSum, CheckType checkType){
 
     char sum[20];
@@ -66,6 +61,16 @@ void Payment::getPaymentSum(double _paymentSum, CheckType checkType){
 
 void Payment::on_lineEdit_cash_money_textChanged(const QString &arg1)
 {
+   layout_of_the_sums();
+}
+
+void Payment::on_lineEdit_bank_money_textChanged(const QString &arg1)
+{
+   layout_of_the_sums();
+}
+
+void Payment::layout_of_the_sums(){
+
     std::string std_be_paid_in_cash = ui->lineEdit_cash_money->text().toStdString();
     std::string std_be_paid_in_bank_ = ui->lineEdit_bank_money->text().toStdString();
 
@@ -103,14 +108,14 @@ void Payment::on_lineEdit_cash_money_textChanged(const QString &arg1)
          return;
     }
 
-    if(be_paid_in_cash_ < paymentSum_ && be_paid_in_bank_ == 0){
-         sprintf (&sum[0], "%.2lf", paymentSum_ - be_paid_in_cash_);
+    if(paymentSum_ > (be_paid_in_cash_ + be_paid_in_bank_) && paymentSum_ - (be_paid_in_cash_ + be_paid_in_bank_) >= 0.009){
+         sprintf (&sum[0], "%.2lf",  paymentSum_ - (be_paid_in_cash_ + be_paid_in_bank_));
          ui->label_form_of_payment->setText(("ЕЩЁ ВНЕСТИ: " + std::string(sum)).c_str());
          return;
     }
 
-    if(be_paid_in_cash_ > paymentSum_ && be_paid_in_bank_ == 0){
-         sprintf (&sum[0], "%.2lf", be_paid_in_cash_ - paymentSum_);
+    if(paymentSum_ < ( be_paid_in_cash_+ be_paid_in_bank_)){
+         sprintf (&sum[0], "%.2lf", ( be_paid_in_cash_+ be_paid_in_bank_) - paymentSum_);
          ui->label_form_of_payment->setText(("СДАЧА: " + std::string(sum)).c_str());
          return;
     }
@@ -129,67 +134,14 @@ void Payment::on_lineEdit_cash_money_textChanged(const QString &arg1)
     }
 }
 
-void Payment::on_lineEdit_bank_money_textChanged(const QString &arg1)
+void Payment::on_pushButton_pay_clicked()
 {
-    std::string std_be_paid_in_cash = ui->lineEdit_cash_money->text().toStdString();
-    std::string std_be_paid_in_bank_ = ui->lineEdit_bank_money->text().toStdString();
-
-    std_be_paid_in_cash = regex_replace(std_be_paid_in_cash, std::regex(","), ".");
-    std_be_paid_in_bank_ = regex_replace(std_be_paid_in_bank_, std::regex(","), ".");
-
-    try {
-        be_paid_in_cash_ = stod(std_be_paid_in_cash);
-
-    } catch (...) {
-        ui->lineEdit_cash_money->setText("0,00");
-        be_paid_in_cash_ = 0;
-    }
-
-    try {
-        be_paid_in_bank_ = stod(std_be_paid_in_bank_);
-
-    } catch (...) {
-        ui->lineEdit_bank_money->setText("0,00");
-         be_paid_in_bank_ = 0;
-    }
-
-    char sum[20];
-    char sum_bank[20];
-
-    if(be_paid_in_cash_ == 0 && be_paid_in_bank_ == 0){
-         sprintf (&sum[0], "%.2lf", paymentSum_);
-         ui->label_form_of_payment->setText(("НАЛИЧНЫМИ: " + std::string(sum)).c_str());
-         return;
-    }
-
-    if(std::abs(be_paid_in_cash_ - paymentSum_) <= 0.009 && be_paid_in_bank_ == 0){
-         sprintf (&sum[0], "%.2lf", paymentSum_);
-         ui->label_form_of_payment->setText(("НАЛИЧНЫМИ: " + std::string(sum)).c_str());
-         return;
-    }
-
-    if(be_paid_in_cash_ < paymentSum_ && be_paid_in_bank_ == 0){
-         sprintf (&sum[0], "%.2lf", paymentSum_ - be_paid_in_cash_);
-         ui->label_form_of_payment->setText(("ЕЩЁ ВНЕСТИ: " + std::string(sum)).c_str());
-         return;
-    }
-
-    if(be_paid_in_cash_ > paymentSum_ && be_paid_in_bank_ == 0){
-         sprintf (&sum[0], "%.2lf", be_paid_in_cash_ - paymentSum_);
-         ui->label_form_of_payment->setText(("СДАЧА: " + std::string(sum)).c_str());
-         return;
-    }
-
-    if(be_paid_in_cash_ == 0 && be_paid_in_bank_ != 0){
-         sprintf (&sum[0], "%.2lf", be_paid_in_bank_);
-         ui->label_form_of_payment->setText(("БЕЗНАЛИЧНЫМИ: " + std::string(sum)).c_str());
-         return;
-    }
-
-    if(be_paid_in_cash_ != 0 && be_paid_in_bank_ != 0){
-         sprintf (&sum[0], "%.2lf", be_paid_in_cash_);
-         sprintf (&sum_bank[0], "%.2lf", be_paid_in_bank_);
-         ui->label_form_of_payment->setText(("НАЛ: " + std::string(sum) + "\nБЕЗНАЛ: " + std::string(sum_bank)).c_str());
-         return;
-    }
+        QMessageBox messageBox(QMessageBox::Warning,
+                    (codec->toUnicode("Неверная сумма оплаты")),
+                    (codec->toUnicode("Косяк")),
+                    QMessageBox::Yes,
+                    this);
+         messageBox.setWindowFlag(Qt::WindowStaysOnTopHint);
+         messageBox.setButtonText(QMessageBox::Yes, (codec->toUnicode("OK")));
+         messageBox.exec();
 }
--- payment.h
+++ payment.h
@@ -2,9 +2,12 @@
 #define PAYMENT_H
 
 #include <QMainWindow>
+#include <QTextCodec>
 #include <QString>
 #include <enumkkm.h>
 #include <regex>
+#include <QMessageBox>
+#include <codecvt>
 
 namespace Ui {
 class Payment;
@@ -19,18 +22,22 @@ public:
     ~Payment();
 
 private slots:
-    void on_pushButton_pay_clicked();
+
     void getPaymentSum(double paymentSum, CheckType checkType);
 
     void on_lineEdit_cash_money_textChanged(const QString &arg1);
 
     void on_lineEdit_bank_money_textChanged(const QString &arg1);
 
+    void on_pushButton_pay_clicked();
+
 private:
     Ui::Payment *ui;
     double paymentSum_ = 0;
     double be_paid_in_cash_ = 0;
     double be_paid_in_bank_ = 0;
+    void layout_of_the_sums();
+    QTextCodec *codec = QTextCodec::codecForName("UTF-8");
 };
 
 #endif // PAYMENT_H
